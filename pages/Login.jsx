3e51073524b538eeb05b1d2c536475302eab0990
kaustav927 <kaustav927@gmail.com>
2022-03-21 19:06:14 -0400

    login update

--- pages/Login.jsx
+++ pages/Login.jsx
@@ -8,26 +8,28 @@ function Login() {
   
       <div className="bg-white border-2 border-slate-800 rounded-2xl shadow-2xl flex flex-col w-96 lg:w-2/3 md:w-3/4 max-w-4xl">
         
-        <div className="h-2/5 p-5">
+        <div className="h-2/5 mt-10">
           <p>Logo</p>
         </div>
 
         <div className="h-3/5 py-2 flex flex-col lg:flex-row md:flex-row">
 
 
-          <div className="  w-5/6 px-20 py-12 mx-auto bg-slate-400 text-white">
+          <div className="  w-5/6 px-10 py-5 mx-auto bg-white text-black">
        
-            <h4> Description</h4>
+            <h4 className="text-lg py-5"> Login With Email </h4>
             
             
-              <div className="flex center bg-white p-2 mb-4 ">
+              <div className="flex center bg-slate p-2 mb-4 rounded border-2 border-black ">
 
-                <input type="email" ></input>
-              </div>
-              <div className="flex center bg-white p-2 ">
+                <input  className="text-black border-2" type="email" placeholder="myEmail@gmail.com" ></input>
+              
+                </div>
+              <div className="flex center bg-slate p-2 mb-4 rounded border-2 border-black">
 
-                <input type="email" ></input>
-              </div>
+                <input className="text-black" type="email" placeholder="" ></input>
+              
+                </div>
          
             <button className="mt-4 border-2 border-slate-300 px-12 py-2 hover:bg-slate-200 hover:text-black">Sign in</button>
           </div>
