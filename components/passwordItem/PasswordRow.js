5ad0879244bec1cf4932f2e673e39c1ecb4caf25
kaustav927 <kaustav927@gmail.com>
2022-03-23 15:49:59 -0400

    Password Status

--- components/passwordItem/PasswordRow.js
+++ components/passwordItem/PasswordRow.js
@@ -3,18 +3,28 @@ import classes from './PasswordItem.module.css';
 import Image from 'next/image';
 import editImage from '../../img/edit.png';
 import deleteImage from '../../img/delete.png';
+import {useState} from 'react';
+
 
 const PasswordRow = (props) => {
-
-  
+    const [isActive, setIsActive]= useState(true);
     
-   
+ 
+   const onDoubleClick = (id)=> {
+    setIsActive(!isActive);
+    console.log("double click",id,isActive);
+  }
+
     return(
         <ul className={classes.passItem}>
             <li className={classes.checkBox}><input type="checkbox"></input></li>
             <li className={classes.passName}>{props.passwordInfo.name}</li>
             <li className={classes.passDecription}>{props.passwordInfo.description}</li>
-            <li className={classes.passStatus}>{props.passwordInfo.status}</li>
+            <li className="mr-12">
+                <button className={`${isActive ? "bg-blue-500 rounded px-4 py-2 text-sm text-white" : "bg-red-500 text-xs rounded px-3.5 py-2 text-xs text-white"}`} onDoubleClick={()=>onDoubleClick(props.passwordInfo.id)}>
+                         {<span>{isActive ? <span>Active</span> : <span>Inactive</span>}</span>}
+                </button>
+            </li>
             <li className={classes.editPass}>
                 <Link className={classes.lin} href= '/passwordsList'>
                     <button onClick={()=>props.onEditClick(props.passwordInfo.id)}>
--- pages/passwordsList.js
+++ pages/passwordsList.js
@@ -5,28 +5,11 @@ import classes from './PasswordsList.module.css';
 import Pagination from '../components/pagination/index';
 import {useState} from 'react';
 import EditModal from '../components/passwordItem/EditModal';
-//dummy passwords list
-// const dummy_passwords = [
-//     {id: 1, name: "Facebook.ca", description: "Lorem ipsum dolor sit", status: "active"},
-//     {id: 2, name: "Amazon.ca", description: "Lorem ipsum dolor sit", status: "active"},
-//     {id: 3, name: "Amazon.ca", description: "Lorem ipsum dolor sit", status: "active"},
-//     {id: 4, name: "Amazon.ca", description: "Lorem ipsum dolor sit", status: "active"},
-//     {id: 5, name: "Amazon.ca", description: "Lorem ipsum dolor sit", status: "active"},
-//     {id: 6, name: "Amazon.ca", description: "Lorem ipsum dolor sit", status: "active"},
-//     {id: 7, name: "Facebook.ca", description: "Lorem ipsum dolor sit", status: "active"},
-//     {id: 8, name: "Facebook.ca", description: "Lorem ipsum dolor sit", status: "active"},
-//     {id: 9, name: "Facebook.ca", description: "Lorem ipsum dolor sit", status: "active"},
-//     {id: 10, name: "Amazon.ca", description: "Lorem ipsum dolor sit", status: "active"},
-//     {id: 11, name: "Amazon.ca", description: "Lorem ipsum dolor sit", status: "active"},
-//     {id: 12, name: "Facebook.ca", description: "Lorem ipsum dolor sit", status: "active"},
-//     {id: 13, name: "Amazon.ca", description: "Lorem ipsum dolor sit", status: "active"},
-//     {id: 14, name: "Amazon.ca", description: "Lorem ipsum dolor sit", status: "active"},
-//     {id: 15, name: "Amazon.ca", description: "Lorem ipsum dolor sit", status: "active"}
-// ]
 
 
 
 const PasswordsList = () => {
+    //dummy passwords list
     const [dummyPasswords,setDummyPasswords] = useState([
         {id: 1, name: "Facebook.ca", description: "Lorem ipsum dolor sit", status: "active"},
         {id: 2, name: "Amazon.ca", description: "Lorem ipsum dolor sit", status: "active"},
@@ -46,7 +29,8 @@ const PasswordsList = () => {
     ]);
     const [openEdit, setEdit]=useState(false)
     const [topMostPassIndex, setTopMostPassIndex] = useState(0); 
-  //saves the index of top most password element on the page
+
+    //saves the index of top most password element on the page
     let pageSize = 6;                                              //number of password elements per page                                      //current password id
     //getting the new index for first password element that should be displayed at top of the page  
     //(after user clicked on the right/left button)
@@ -59,12 +43,11 @@ const PasswordsList = () => {
 
     //splits the part of data that is going to be displayed in the other pages
     const otherPagesPass = paginate(dummyPasswords, topMostPassIndex, pageSize);
-    
+
     const onEditClick = (id) => {
         console.log(id);
         setEdit(true)
     }
-
     const onEditClose = (id) => {
         console.log('close' ,id)
         setEdit(false)
@@ -79,36 +62,27 @@ const PasswordsList = () => {
             setEdit(true)
         }
     }
-
     const onDelete= (id) => {
         console.log('delete',id)
         setDummyPasswords(dummyPasswords.filter((pass) => pass.id !== id))
     }
-
-
     return(
-        
         <Layout footerComponent={ <Pagination topMostIndexHandler={topMostIndexHandler} passwordsInfo={dummyPasswords} pageSize={pageSize} address={'passwordsList'} topMostPassIndex={topMostPassIndex}/>}>
-        {openEdit ? <EditModal isOpen={openEdit} onEditClose={onEditClose}/> :
-        <ul className={classes.passwordTable}>  
-                <li className={classes.header}>
-                    <ul>
-                        <li className={classes.passName}>Name<button></button></li>
-                        <li className={classes.passDecription}>Description</li>
-                        <li className={classes.passStatus}>Status</li>
-                        <li className={classes.options}> . . . </li>
-                    </ul>    
-                </li>      
-                {(topMostPassIndex == 0) && (firstPagePass.map(listItem => <li key={listItem.id} className={classes.program}><PasswordRow passwordInfo={listItem} onEditClick={onEditClick} onEditSave={onEditSave} onDelete={onDelete}></PasswordRow></li>))}
-                {(0 < topMostPassIndex) && (topMostPassIndex < dummyPasswords.length) && (otherPagesPass.map(listItem => <li key={listItem.id} className={classes.program}><PasswordItem passwordInfo={listItem}></PasswordItem></li>))}             
-            </ul>
-        }
-        
-            
-         
+            {openEdit ? <EditModal isOpen={openEdit} onEditClose={onEditClose}/> :
+            <ul className={classes.passwordTable}>  
+                    <li className={classes.header}>
+                        <ul>
+                            <li className={classes.passName}>Name<button></button></li>
+                            <li className={classes.passDecription}>Description</li>
+                            <li className={classes.passStatus}>Status</li>
+                            <li className={classes.options}> . . . </li>
+                        </ul>    
+                    </li>      
+                    {(topMostPassIndex == 0) && (firstPagePass.map(listItem => <li key={listItem.id} className={classes.program}><PasswordRow passwordInfo={listItem} onEditClick={onEditClick} onEditSave={onEditSave} onDelete={onDelete}></PasswordRow></li>))}
+                    {(0 < topMostPassIndex) && (topMostPassIndex < dummyPasswords.length) && (otherPagesPass.map(listItem => <li key={listItem.id} className={classes.program}><PasswordItem passwordInfo={listItem}></PasswordItem></li>))}             
+                </ul>
+            }
         </Layout>
-        
-     
     );
 };
 
