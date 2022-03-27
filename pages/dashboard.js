037560723e7ffa0da48ae99256d72d61e92626e5
Amanpreet Gill <amanpreetg100@gmail.com>
2022-03-26 20:25:38 -0400

    updated dashboard piechart

--- pages/dashboard.js
+++ pages/dashboard.js
@@ -1,13 +1,28 @@
 import Layout from '../components/layout/Layout';
+import Image from 'next/image';
+import mypic from '../img/dash_piechart.png';
+import styles from "./dashboard.module.css";
+
+    // set the data
+    var data = {
+    header: ["Password Strength", "Passwords"],
+    rows: [
+      ["Strong", 1500],
+      ["Medium", 87000],
+      ["Weak", 175000]
+    ]};
+
 
 const Dashboard = () => {
 
     return(
         <Layout>
-            <h3>Dashboard</h3>
+            <div className={styles.image}><Image src={mypic} alt="Simple pie chart of number of users"/></div>
+        
+            <div></div>
+
         </Layout>
-        
-        
+          
     );
 };
 
