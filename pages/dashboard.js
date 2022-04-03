2869fbec925746416068358c5bc3aa21c9cab5f6
Amanpreet <amanpreetg100@gmail.com>
2022-04-03 17:52:33 -0400

    Revert "finished up dashboard"
    This reverts commit d412ec905f5410be357f8c7522be0f9b6affdb8f.

--- pages/dashboard.js
+++ pages/dashboard.js
@@ -2,57 +2,24 @@ import Layout from '../components/layout/Layout';
 import Image from 'next/image';
 import mypic from '../img/dash_piechart.png';
 import styles from "./dashboard.module.css";
-import React, { useState, useCallback } from "react";
-import {BarChart, Bar, XAxis, YAxis, CartesianGrid, Tooltip, Legend} from "recharts";
 
     // set the data
-    const data = [
-      {
-        name: "Strong",
-        passwords: 7
-      },
-      {
-        name: "Medium",
-        passwords: 4
-      },
-      {
-        name: "Weak",
-        passwords: 4
-      },
-      
-    ];
-  
+    var data = {
+    header: ["Password Strength", "Passwords"],
+    rows: [
+      ["Strong", 1500],
+      ["Medium", 87000],
+      ["Weak", 175000]
+    ]};
+
 
 const Dashboard = () => {
 
     return(
         <Layout>
-            {/* this is the pie chart image */}
             <div className={styles.image}><Image src={mypic} alt="Simple pie chart of number of users"/></div>
-            
-            {/* the following handles the bar graph */}
-            <div className={styles.Graph}>
-              <h1>Ratio of Password Strength</h1>
-              <BarChart
-                width={600}
-                height={400}
-                data={data}
-                margin={{
-                  top: 2,
-                  right: 50,
-                  left: 20,
-                  bottom: 5
-                }}
-                barSize={30}
-              >
-                <XAxis dataKey="name" scale="point" padding={{ left: 30, right: 10 }} />
-                <YAxis />
-                <Tooltip />
-                <Legend />
-                <CartesianGrid strokeDasharray="3 3" />
-                <Bar dataKey="passwords" fill="#8884d8" background={{ fill: "#eee" }} />
-              </BarChart>
-            </div>
+        
+            <div></div>
 
         </Layout>
           
